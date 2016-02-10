4347f73bed3b61db3da341c8fac1a5fd4be09a06
Murat Cakmak <oguzhanmc@gmail.com>
2016-02-10 15:04:02 -0500

    The multiple choices mechanism is working.

--- MultipleChoiceQuestions/ViewController.swift
+++ MultipleChoiceQuestions/ViewController.swift
@@ -8,18 +8,68 @@
 
 import UIKit
 
+struct Question {
+    var question: String
+    var choices = [String]()
+    var answer: Int!
+}
+
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet var choices: [UIButton]!
+    
+    var questions = [Question]()
+    
+    var correctChoice = Int()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        questions = [Question(question: "En güzel kız kimdir?", choices: ["Irem", "Irem Az", "Elif Irem Az", "Elif Irem Az Cakmak"], answer: 3)]
+        
+        for question in questions{
+            presentQuestion(question)
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func presentQuestion(question: Question){
+        questionLabel.text = question.question
+        
+        for index in 0..<choices.count{
+            print(index)
+            choices[index].setTitle("\(question.choices[index])", forState: UIControlState.Normal)
+        }
+        self.correctChoice = question.answer
     }
 
+    @IBAction func aButton(sender: AnyObject) {
+        isCorrectAnswer(1, correctChoice: correctChoice)
+        
+    }
 
+    @IBAction func bButton(sender: AnyObject) {
+    
+    }
+    
+    @IBAction func cButton(sender: AnyObject) {
+    
+    }
+    
+    @IBAction func dButton(sender: AnyObject) {
+        isCorrectAnswer(3, correctChoice: correctChoice)
+    }
+    
+    func isCorrectAnswer(usersChoice: Int, correctChoice:Int){
+        if(usersChoice == correctChoice){
+            NSLog("Well Done!")
+        }else{
+            NSLog("Wrong")
+        }
+    }
 }
 
